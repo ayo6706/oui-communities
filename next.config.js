0bf69476e795f21292ae5fb1f58baef2a7f3e7b5
repotecc <repotecc@gmail.com>
2021-12-03 01:33:23 +0100

    init

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  redirects() {
+    return [
+      process.env.MAINTENANCE_MODE === "1"
+        ? { source: "/((?!maintenance).*)", destination: "/maintenance.html", permanent: false }
+        : null,
+    ].filter(Boolean);
+  }
+};
